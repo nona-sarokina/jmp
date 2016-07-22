fcae90804a8911159dcfb8c7036b70358212e53b
nsorokina <forgakk@gmail.com>
2016-07-22 19:33:48 +0300

    jmp - module3 - task1 - Superman adding

---
+++ src/main/java/module3/task7/Runner.java
@@ -0,0 +1,16 @@
+package module3.task7;
+
+import module3.task7.Superman;
+
+/**
+ * Created by user on 22.07.2016.
+ */
+public class Runner {
+    public static void main(String[] args) {
+
+        Superman.INSTANCE.rescueKitten();
+        Superman.INSTANCE.saveTheWorld();
+        Superman.INSTANCE.afraidOfKryptonite();
+
+    }
+}
---
+++ src/main/java/module3/task7/Superman.java
@@ -0,0 +1,20 @@
+package module3.task7;
+
+/**
+ * Created by user on 22.07.2016.
+ */
+public enum Superman {
+    INSTANCE;
+
+    public void rescueKitten() {
+        System.out.println("rescuing kitten");
+    }
+
+    public void saveTheWorld() {
+        System.out.println("saving the world");
+    }
+
+    public void afraidOfKryptonite () {
+        System.out.println("AAAAAA, Kryptonite! Panic!");
+    }
+}
