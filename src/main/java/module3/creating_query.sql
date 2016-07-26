30b260a2180c5c1c894707f2dc8ee9454acbc647
Nona Sarokina <nona.sarokina@datalex.com>
2016-07-26 09:37:14 +0300

    jmp - module3- task7 - creating database query changes

--- src/main/java/module3/creating_query.sql
+++ src/main/java/module3/creating_query.sql
@@ -3,7 +3,6 @@ CREATE TABLE PERSONS
 (
   id INT PRIMARY KEY AUTO_INCREMENT,
   name VARCHAR(50) NOT NULL,
-  lastname VARCHAR(50) NOT NULL,
   dateOfBirth DATE NOT NULL,
   address VARCHAR(100) NOT NULL,
   city VARCHAR(20) NOT NULL,
@@ -12,8 +11,8 @@ CREATE TABLE PERSONS
 
 
 INSERT INTO Persons (name, dateOfBirth, address, city, zipcode) VALUES
-  ('Isaak', 'Cleve', '1984-01-28', '1104 Sun Valley Road', 'Wishram', 35078),
-  ('Russel', 'Firuz', '1983-11-19', '2906 Tully Street', 'Detroit', 95350),
-  ('August', 'Walherich', '1980-01-11', '3744 Spirit Drive', 'Jacksonville', 55109),
-  ('Dan', 'Manfred', '1980-01-07', '3692 Wescam Court', 'Fallon', 26330),
-  ('Stjepan', 'Alpertti', '1986-05-03', '3384 Marshall Street', 'Beltsville', 32301);
+  ('Isaak Cleve', '1984-01-28', '1104 Sun Valley Road', 'Wishram', 35078),
+  ('Russel Firuz', '1983-11-19', '2906 Tully Street', 'Detroit', 95350),
+  ('August Walherich', '1980-01-11', '3744 Spirit Drive', 'Jacksonville', 55109),
+  ('Dan Manfred', '1980-01-07', '3692 Wescam Court', 'Fallon', 26330),
+  ('Stjepan Alpertti', '1986-05-03', '3384 Marshall Street', 'Beltsville', 32301);
